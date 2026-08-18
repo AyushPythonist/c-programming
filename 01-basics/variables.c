5c38d68aceb0991fe6205b6d2aadf177b8b3cdd9
Code With Ayush <aistoryteller109@gmail.com>
2026-08-18 20:27:50 +0530

    Create variables.c
    Add variables program

---
+++ 01-basics/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 22;
+    float marks = 45.55;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Marks: %.2f\n", marks);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
