a6a394a7060789f5b5aed00b2a3e3935262d8657
Code With Ayush <aistoryteller109@gmail.com>
2026-08-19 23:25:52 +0530

    Create DataTypes.c
    Basic C data types and their usage with examples.

---
+++ 01-basics/DataTypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 22;
+    float marks = 85.5;
+    double pi = 3.1415926535;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Marks: %.2f\n", marks);
+    printf("Pi: %.10lf\n", pi);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
